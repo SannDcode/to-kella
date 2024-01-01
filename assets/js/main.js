30d733d58aeb457078618f2041c69a88c8a650e6
SannDcode <108715412+SannDcode@users.noreply.github.com>
2024-01-02 05:00:57 +0700

    main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,27 @@
 onload = () => {
-        document.body.classList.remove("container");
-    };
-    
-    const myAudio = document.getElementById('myAudio');
-    myAudio.play();
+    document.body.classList.remove("container");
+};
 
-    function openWhatsAppChat(message) {
-        const phoneNumber = '087871537199'; // Ganti dengan nomor telepon yang diinginkan
-    
-        window.open(`https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`, '_blank');
+const myAudio = document.getElementById('myAudio');
+myAudio.play();
+
+function openWhatsAppChat(message) {
+    const phoneNumber = '087871537199'; // Ganti dengan nomor telepon yang diinginkan
+
+    window.open(`https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`, '_blank');
+}
+
+Swal.fire({
+    title: 'Bagus ga? ehehe',
+    html: '<input id="whatsappMessage" class="swal2-input" placeholder="Ketik pesan Anda...">', // Input field untuk pesan kustom
+    icon: 'info',
+    showCancelButton: true,
+    confirmButtonText: 'Kirim',
+    cancelButtonText: 'Batal',
+    preConfirm: () => {
+        const message = document.getElementById('whatsappMessage').value;
+        setTimeout(() => {
+            openWhatsAppChat(message);
+        }, 4000); // Jeda waktu 2 detik sebelum memanggil openWhatsAppChat
     }
-    setTimeout(() => {
-        Swal.fire({
-            title: 'Bagus ga? ehehe',
-            html: '<input id="whatsappMessage" class="swal2-input" placeholder="Ketik pesan Anda...">', // Input field untuk pesan kustom
-            icon: 'info',
-            showCancelButton: true,
-            confirmButtonText: 'Kirim',
-            cancelButtonText: 'Batal',
-            preConfirm: () => {
-                const message = document.getElementById('whatsappMessage').value;
-                openWhatsAppChat(message);
-            }
-        });
-    }, 5000); // Jeda waktu 2 detik sebelum muncul SweetAlert
-    
+});
